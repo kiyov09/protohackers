56b9b553cb636a76fc7bd3d0fa4bd1aaa96fe9fd
Enrique Mejías Álvarez <enrique.mejias@envato.com>
2023-03-24 07:17:33 -0600

    Some changes 😅

--- src/smoke_test_0/main.rs
+++ src/smoke_test_0/main.rs
@@ -1,22 +1,22 @@
 use std::io::prelude::*;
 use std::net::{SocketAddr, TcpListener, TcpStream};
 
-fn handle_connection(mut stream: TcpStream) -> Result<(), std::io::Error> {
+fn handle_connection(mut stream: TcpStream) {
     let mut buff = [0u8; 1024];
 
     while let Ok(bytes_read) = stream.read(&mut buff) {
         if bytes_read == 0 {
             break;
         }
-        stream.write_all(&buff[..bytes_read])?;
-        buff = [0; 1024];
-    }
 
-    Ok(())
+        if stream.write(&buff[..bytes_read]).is_err() {
+            break;
+        }
+    }
 }
 
 fn main() -> std::io::Result<()> {
-    let socket_add = SocketAddr::from(([127, 0, 0, 1], 7878));
+    let socket_add = SocketAddr::from(([0, 0, 0, 0], 5000));
     let listener = TcpListener::bind(socket_add).unwrap();
 
     let thread_pool = rayon::ThreadPoolBuilder::new()
@@ -32,7 +32,7 @@ fn main() -> std::io::Result<()> {
 
         thread_pool.spawn(move || {
             println!("Connection established!");
-            handle_connection(stream).expect("Failed to handle connection");
+            handle_connection(stream);
         });
     }
 
