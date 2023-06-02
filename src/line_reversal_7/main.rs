7826c5cd9c9d5ef021ec903d59976403ce3777cc
Enrique Mejías Álvarez <enrique.mejias@envato.com>
2023-06-02 11:51:55 -0600

    Comments and refactors

--- src/line_reversal_7/main.rs
+++ src/line_reversal_7/main.rs
@@ -26,6 +26,16 @@ where
     }
 }
 
+fn get_positive_i32(s: &str) -> Result<i32, InvalidMessage> {
+    s.parse::<i32>().map_err(|_| InvalidMessage).and_then(|i| {
+        if i.is_negative() {
+            Err(InvalidMessage)
+        } else {
+            Ok(i)
+        }
+    })
+}
+
 #[derive(Debug)]
 struct Connect {
     session: usize,
@@ -39,13 +49,7 @@ impl FromStr for Connect {
             return Err(InvalidMessage);
         }
 
-        let session_id = s[..s.len() - 1]
-            .parse::<i32>()
-            .map_err(|_| InvalidMessage)?;
-
-        if session_id.is_negative() {
-            return Err(InvalidMessage);
-        }
+        let session_id = get_positive_i32(&s[..s.len() - 1])?;
 
         Ok(Connect {
             // Till length - 1 to remove the trailing slash
@@ -62,13 +66,21 @@ struct Data {
 }
 
 impl Data {
+    // Methods
+    async fn send(&self, addr: SocketAddr, socket: &UdpSocket) -> io::Result<()> {
+        socket
+            .send_to(self.into_response().as_bytes(), addr)
+            .await
+            .map(|_| ())
+    }
+
+    // Associated Functions
+
     fn unescape(s: &str) -> String {
-        // Replace \\ with \ and \/ with /
         s.replace("\\\\", "\\").replace("\\/", "/")
     }
 
     fn escape(s: &str) -> String {
-        // Replace \ with \\ and / with \/
         s.replace('\\', "\\\\").replace('/', "\\/")
     }
 
@@ -87,13 +99,6 @@ impl Data {
             .map(|line| Self::escape(&line))
             .map(|line| line + "\n")
             .collect::<String>()
-    }
-
-    async fn send(&self, addr: SocketAddr, socket: &UdpSocket) -> io::Result<()> {
-        socket
-            .send_to(self.into_response().as_bytes(), addr)
-            .await
-            .map(|_| ())
     }
 
     fn is_valid(data: &str) -> bool {
@@ -134,26 +139,8 @@ impl FromStr for Data {
 
         let mut parts = s[..s.len() - 1].splitn(3, '/');
 
-        let session = parts
-            .next()
-            .ok_or(InvalidMessage)?
-            .parse::<i32>()
-            .map_err(|_| InvalidMessage)?;
-
-        if session.is_negative() {
-            return Err(InvalidMessage);
-        }
-
-        let pos = parts
-            .next()
-            .ok_or(InvalidMessage)?
-            .parse::<i32>()
-            .map_err(|_| InvalidMessage)?;
-
-        if pos.is_negative() {
-            return Err(InvalidMessage);
-        }
-
+        let session = get_positive_i32(parts.next().ok_or(InvalidMessage)?)?;
+        let pos = get_positive_i32(parts.next().ok_or(InvalidMessage)?)?;
         let data = parts.next().ok_or(InvalidMessage)?;
 
         if Self::is_valid(data) {
@@ -178,8 +165,8 @@ impl Ack {
     async fn send(&self, addr: SocketAddr, socket: &UdpSocket) -> io::Result<()> {
         socket
             .send_to(self.into_response().as_bytes(), addr)
-            .await?;
-        Ok(())
+            .await
+            .map(|_| ())
     }
 }
 
@@ -192,26 +179,8 @@ impl FromStr for Ack {
         }
 
         let mut parts = s.split('/').take(2);
-
-        let session = parts
-            .next()
-            .ok_or(InvalidMessage)?
-            .parse::<i32>()
-            .map_err(|_| InvalidMessage)?;
-
-        if session.is_negative() {
-            return Err(InvalidMessage);
-        }
-
-        let length = parts
-            .next()
-            .ok_or(InvalidMessage)?
-            .parse::<i32>()
-            .map_err(|_| InvalidMessage)?;
-
-        if length.is_negative() {
-            return Err(InvalidMessage);
-        }
+        let session = get_positive_i32(parts.next().ok_or(InvalidMessage)?)?;
+        let length = get_positive_i32(parts.next().ok_or(InvalidMessage)?)?;
 
         Ok(Ack {
             session: session as usize,
@@ -235,8 +204,8 @@ impl Close {
     async fn send(&self, addr: SocketAddr, socket: &UdpSocket) -> io::Result<()> {
         socket
             .send_to(self.into_response().as_bytes(), addr)
-            .await?;
-        Ok(())
+            .await
+            .map(|_| ())
     }
 }
 
@@ -254,13 +223,7 @@ impl FromStr for Close {
             return Err(InvalidMessage);
         }
 
-        let session_id = s[..s.len() - 1]
-            .parse::<i32>()
-            .map_err(|_| InvalidMessage)?;
-
-        if session_id.is_negative() {
-            return Err(InvalidMessage);
-        }
+        let session_id = get_positive_i32(&s[..s.len() - 1])?;
 
         Ok(Close {
             session: session_id as usize,
@@ -271,6 +234,7 @@ impl FromStr for Close {
 #[derive(Debug)]
 struct InvalidMessage;
 
+/// Messages that we can receive from the client.
 #[derive(Debug)]
 enum Message {
     Connect(Connect), // Ex: /connect/SESSION/
@@ -321,11 +285,12 @@ impl FromStr for Message {
     }
 }
 
-// Time to wait before resend a message to the client (in seconds)
+/// Time to wait before resend a message to the client (in seconds)
 const RETRY_TIMEOUT: u64 = 5;
-// Time to wait before consider a session expired (in seconds)
+/// Time to wait before consider a session expired (in seconds)
 const EXPIRE_TIMEOUT: u64 = 30;
 
+/// A client's session.
 #[derive(Debug, Clone)]
 struct Session {
     id: usize,
@@ -337,7 +302,7 @@ struct Session {
     curr_pos: usize,
     // Messages sent to the client that have not been acked yet
     send_data: HashMap<usize, (Instant, Data)>,
-    // Maybe I dont need this
+    // TODO: Maybe I dont need this
     all_sent_pos: Vec<usize>,
     // Sender to communicate with the server thread
     sender: Sender<ServerMsg>,
@@ -347,25 +312,26 @@ impl Session {
     async fn handle_ack(&mut self, ack: Ack) {
         let larger = self.all_sent_pos.iter().max().unwrap_or(&0);
 
+        // Check if ack is valid and close session if it is not
         if larger < &ack.length {
             // Missbehaving client
-            return self
-                .sender
-                .send(ServerMsg::ToClient(
-                    Message::Close(Close { session: self.id }),
-                    self.peer_addr,
-                ))
-                .await
-                .expect("Failed to send to client");
+            return self.send(Message::Close(Close { session: self.id })).await;
         }
 
+        // Checking if we need to retransmit data
         if larger > &ack.length {
+            // TODO:
+            // This code has a lot of duplication with the *handle_data* method.
+            // I need to review how to refactor this.
+
+            // Get data after the acked position and reverse it for retransmission
             let data_after_length = self.payload[ack.length..].to_string();
             let reversed_data = Data::process_for_response(&data_after_length);
 
+            // To be used as the marker for the next data to be sent
             let mut pos = ack.length;
 
-            // Send message to client in chunks of 500 bytes + header
+            // Send message to client in chunks of 750 bytes
             for chunk in reversed_data.chars().collect::<Vec<_>>().chunks(750) {
                 // Send data to client
                 let retrans_data = Data {
@@ -380,61 +346,53 @@ impl Session {
                     .insert(pos, (Instant::now(), retrans_data.clone()));
                 self.all_sent_pos.push(pos);
 
-                self.sender
-                    .send(ServerMsg::ToClient(
-                        Message::Data(retrans_data.clone()),
-                        self.peer_addr,
-                    ))
-                    .await
-                    .expect("Failed to send to client");
+                // Send to client
+                self.send(Message::Data(retrans_data.clone())).await;
             }
 
             return;
         }
 
+        // Remove the data that was acked from the list of data to be retransmitted
         self.send_data.remove(&ack.length);
     }
 
     async fn handle_data(&mut self, d: Data) {
+        // Ignore any data message that is sending data from a position that we already have
         if d.pos != self.payload.len() {
+            // Let the client know that we already have that data
             return self
-                .sender
-                .send(ServerMsg::ToClient(
-                    Message::Ack(Ack {
-                        session: self.id,
-                        length: self.payload.len(),
-                    }),
-                    self.peer_addr,
-                ))
-                .await
-                .expect("Failed to send to client");
+                .send(Message::Ack(Ack {
+                    session: self.id,
+                    length: self.payload.len(),
+                }))
+                .await;
         }
 
+        // Unscape data and add it to the payload
         let incoming_data = Data::unescape(&d.data);
         self.payload.push_str(&incoming_data);
 
-        self.sender
-            .send(ServerMsg::ToClient(
-                Message::Ack(Ack {
-                    session: self.id,
-                    length: self.payload.len(),
-                }),
-                self.peer_addr,
-            ))
-            .await
-            .expect("Failed to send to client");
+        // Let the client know that we received the data
+        self.send(Message::Ack(Ack {
+            session: self.id,
+            length: self.payload.len(),
+        }))
+        .await;
 
+        // Process data to be sent back to the client
         let reversed_data = Data::process_for_response(&self.payload[self.curr_pos..]);
 
+        // As we sent only complete lines, if the data is empty means that there
+        // is no complete line from the current position, so we dont need to
+        // send anything back
         if reversed_data.is_empty() {
             return;
         }
 
-        let mut to_send = Vec::new();
-
-        // Send message to client in chunks of 500 bytes + header
+        // Send message to client in chunks of 750 bytes
         for chunk in reversed_data.chars().collect::<Vec<_>>().chunks(750) {
-            // Send data to client
+            // Data for the client
             let data_for_client = Data {
                 session: self.id,
                 pos: self.curr_pos,
@@ -443,23 +401,24 @@ impl Session {
 
             // Save for retransmission
             self.curr_pos += chunk.len();
-
             self.send_data
                 .insert(self.curr_pos, (Instant::now(), data_for_client.clone()));
             self.all_sent_pos.push(self.curr_pos);
 
-            to_send.push(data_for_client);
+            // Send reversed data back
+            self.send(Message::Data(data_for_client)).await;
         }
+    }
 
-        for (idx, data) in to_send.into_iter().enumerate() {
-            self.sender
-                .send(ServerMsg::ToClient(Message::Data(data), self.peer_addr))
-                .await
-                .expect("Failed to send to client");
-        }
+    async fn send(&self, msg: Message) {
+        self.sender
+            .send(ServerMsg::ToClient(msg, self.peer_addr))
+            .await
+            .expect("Failed to send to client");
     }
 }
 
+/// Internal messages for the threads to communicate with the server
 #[derive(Debug)]
 enum ServerMsg {
     Socket(Message, SocketAddr),
@@ -468,6 +427,8 @@ enum ServerMsg {
     ToClient(Message, SocketAddr),
 }
 
+/// Server struct, this handles all the communication with the clients
+/// and the retransmission of data.
 struct Server {
     // Map session id to Session data and the last time it was active
     sessions: HashMap<usize, (Session, Instant)>,
@@ -481,8 +442,9 @@ struct Server {
 }
 
 impl Server {
+    /// Creates a new server from a UdpSocket
     fn new(socket: UdpSocket) -> Self {
-        let (sd, rc) = channel::<ServerMsg>(1000000);
+        let (sd, rc) = channel::<ServerMsg>(100);
 
         Self {
             sessions: HashMap::new(),
@@ -492,21 +454,30 @@ impl Server {
         }
     }
 
+    /// Put the server to work
     async fn run(&mut self) {
+        // Spawn threads for activity checking and retransmission of data
+        // TODO:
+        // I need to find a way to stop this threads when they have nothing to do
+        // and respawn them when needed.
         let _handler = self.spawn_activity(self.sender.clone()).await;
         let _handler = self.spawn_retrans(self.sender.clone()).await;
 
+        // Spawn thread to handle incoming messages from clients
         self.spawn_socket(self.sender.clone()).await;
 
         while let Some(msg) = self.receiver.recv().await {
             match msg {
+                // Message from the socket (network connections)
                 ServerMsg::Socket(msg, addr) => match msg {
                     Message::Connect(conn) => self.handle_connect(conn.session, addr).await,
                     Message::Data(data) => self.handle_data(data, addr).await,
                     Message::Ack(ack) => self.handle_ack(ack, addr).await,
                     Message::Close(close) => self.handle_close(close, addr).await,
                 },
+                // Activity check
                 ServerMsg::Activity => {
+                    // TODO: Move this to its own method
                     let now = Instant::now();
                     let mut to_remove = Vec::new();
 
@@ -526,7 +497,9 @@ impl Server {
                             .await;
                     }
                 }
+                // Retransmission of data
                 ServerMsg::Retrans => {
+                    // TODO: Move this to its own method
                     let now = Instant::now();
                     for (sess, _) in self.sessions.values_mut() {
                         for (_, (last, data)) in sess.send_data.iter() {
@@ -538,14 +511,17 @@ impl Server {
                         }
                     }
                 }
+                // Messages comming from the sessions (to send to the clients)
                 ServerMsg::ToClient(msg, addr) => match msg {
+                    // A session will never send a connect message
                     Message::Connect(_) => unreachable!("Invalid message"),
-                    Message::Data(d) => d.send(addr, &self.socket).await.expect("Failed to send"),
-                    Message::Ack(a) => a.send(addr, &self.socket).await.expect("Failed to send"),
-                    Message::Close(c) => {
-                        self.sessions.remove(&c.session);
-                        c.send(addr, &self.socket).await.expect("Failed to send")
+                    Message::Data(data) => {
+                        data.send(addr, &self.socket).await.expect("Failed to send")
                     }
+                    Message::Ack(ack) => {
+                        ack.send(addr, &self.socket).await.expect("Failed to send")
+                    }
+                    Message::Close(close) => self.handle_close(close, addr).await,
                 },
             }
         }
@@ -622,53 +598,60 @@ impl Server {
 
     async fn handle_close(&mut self, close: Close, addr: SocketAddr) {
         self.sessions.remove(&close.session);
-        Close {
-            session: close.session,
-        }
-        .send(addr, &self.socket)
-        .await
-        .expect("Failed to send close")
+        close
+            .send(addr, &self.socket)
+            .await
+            .expect("Failed to send close")
     }
 
     async fn handle_data(&mut self, data: Data, addr: SocketAddr) {
+        // Get the session associated with the message or close the connection if it
+        // has not been open yet
         let Some((sess, inst)) = self.sessions.get_mut(&data.session) else {
             return Close {
                 session: data.session,
             }.send(addr, &self.socket).await.expect("Failed to send close");
         };
 
+        // Update the last activity time
         *inst = Instant::now();
+
+        // Let the session handle the data
         sess.handle_data(data).await
     }
 
     async fn handle_ack(&mut self, ack: Ack, addr: SocketAddr) {
+        // Get the session associated with the message or close the connection if it
+        // has not been open yet
         let Some((sess, inst)) = self.sessions.get_mut(&ack.session) else {
             return Close {
                 session: ack.session,
             }.send(addr, &self.socket).await.expect("Failed to send close");
         };
 
+        // Update the last activity time
         *inst = Instant::now();
+
+        // Let the session handle the ack
         sess.handle_ack(ack).await
     }
 }
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    // For running locally
     // let socket_add = std::net::SocketAddr::from(([0, 0, 0, 0], 8080));
 
     // For use with fly.io
     // See [here](https://fly.io/docs/app-guides/udp-and-tcp/) for more info
     let socket_add = "fly-global-services:5000";
+
+    // Create the socket
     let sock = UdpSocket::bind(socket_add).await?;
 
+    // Start the server
     let mut server = Server::new(sock);
     server.run().await;
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
 }
